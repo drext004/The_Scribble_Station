52c3e733bccc2c18835c44a2e2233006478b70cf
drext004 <atharvdhari217@gmail.com>
2025-07-04 00:26:42 +0530

    Icon changes, forms addition

--- src/create.js
+++ src/create.js
@@ -1,7 +1,56 @@
+import { useState } from "react";
+
 const Create = () => {
+    const[title, setTitle] = useState('');
+    const[body, setBody] = useState('');
+    const[author, setAuthor] = useState('mario');
+    const[insPending, setIsPending]= useState(false);
+
+
+    const handleSubmit= (e)=>{
+        e.preventDefault();
+        const blog={title, body, author};
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method:'POST',
+            headers: {"content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }) .then(()=>{
+            setIsPending(false);
+        })
+
+    }
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=> setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                required
+                value={body}
+                onChange={(e)=>setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                {!insPending && <button>Add Blog</button>}
+                {insPending && <button disabled>Adding blog...</button>}
+            </form>
         </div>
      );
 }
